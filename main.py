c7f7bc287c93a350f3fd8e6929f01882101fa151
John4064 <jparkhurst120@gmail.com>
2020-08-11 20:16:21 -0400

    Quick Update
    Just cleaned up test gui. Empty Now Tomorrow we start implementing the design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import Data
 import tkinter as tk
-
+import panda
 
 #This Checks to see the greatest gain from the opening of the day to the closing
 #Returns a list of indexs of the specified days
@@ -34,37 +34,12 @@ def greatestGain(stocks):
             if(stocks[x]["high"][g]>25):
                 print(stocks[x]["high"][g])
 def damn():
-    print("DICK WAD")
+    tkMessageBox.showinfo("Hello Python", "Hello World")
     return
 if __name__ == "__main__":
     gui = tk.Tk()
     gui.geometry("800x600")
     gui.resizable(width=False, height=False)
-    frame = tk.Frame(gui, bg ="blue")
-    frame.pack()
-    bottomframe = tk.Frame(gui)
-    bottomframe.pack(side=tk.BOTTOM)
-    leftFrame = tk.Frame(gui)
-    leftFrame.pack(side=tk.LEFT)
-    rightFrame = tk.Frame(gui)
-    rightFrame.pack(side=tk.RIGHT)
 
-    button = tk.Button(frame,text = "QUIT", bg = "cyan",fg ="gray",command=quit,bd = 6)
-    button.pack(side=tk.LEFT)
-    button1 = tk.Button(bottomframe, text="DAN", bg="cyan", fg="gray", command=quit, bd=6)
-    button1.pack(side=tk.LEFT)
-
-
-
-
-    w = tk.Menubutton(leftFrame,text="DICK",relief = tk.RAISED, bitmap="@quit.xbm",bg="cyan")
-    w.menu = tk.Menu(w,tearoff = 0)
-    w.grid()
-
-    dan = tk.IntVar()
-    ketch= tk.IntVar()
-    w.menu.add_checkbutton(label="dan", variable=dan)
-    w.menu.add_checkbutton(label="ketch", variable=ketch,command=quit)
-    w.pack()
     #Execution of GUI
     gui.mainloop()
